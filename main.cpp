f9ae8109402302ba9ac5b8e4ac071001263d3357
405210048 <52111767+405210048@users.noreply.github.com>
2019-06-23 04:03:48 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#define SIZE 100
+using namespace std;
+
+int main(void)
+{
+    int mat[SIZE][SIZE];
+    int a, b, maxx=0;int i, j;
+    while(1)
+    {
+        cin>>a>>b;
+        for(i=0;i<a;i++)
+            for(j=0;j<b;j++)
+            cin>>mat[i][j];
+        for(j=0;j<b;j++)
+        {
+            for(i=0;i<a;i++)
+            {
+                cout<<mat[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+}
+}
